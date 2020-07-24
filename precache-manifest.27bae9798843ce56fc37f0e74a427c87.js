0976137b894f6ae845a825b0e5670c0289989d87
Michael Douglas <michaeldouglas@Mac-mini-de-Michael.local>
2020-07-24 15:34:16 -0300

    Updates

---
+++ precache-manifest.27bae9798843ce56fc37f0e74a427c87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912f9de3b3bba90e6c3a63e2ee70ad27",
+    "url": "/cronograma-capilar/index.html"
+  },
+  {
+    "revision": "87d528537edec9f22f0d",
+    "url": "/cronograma-capilar/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a8681494581f9d99d34",
+    "url": "/cronograma-capilar/static/css/main.18fdf9cd.chunk.css"
+  },
+  {
+    "revision": "87d528537edec9f22f0d",
+    "url": "/cronograma-capilar/static/js/2.54843d23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cronograma-capilar/static/js/2.54843d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8681494581f9d99d34",
+    "url": "/cronograma-capilar/static/js/main.a083b962.chunk.js"
+  },
+  {
+    "revision": "254aef202f59f4776498",
+    "url": "/cronograma-capilar/static/js/runtime-main.46a91ee6.js"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/cronograma-capilar/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "09ec9ad50689ea3a751ce05d6e588cfd",
+    "url": "/cronograma-capilar/static/media/book.09ec9ad5.png"
+  },
+  {
+    "revision": "ad05ea2d27195e83b3bc12ef8b8cd7d1",
+    "url": "/cronograma-capilar/static/media/border-img.ad05ea2d.png"
+  }
+]);
